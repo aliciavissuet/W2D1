7b220fe2eb741e700da10de4f9c73871c57a1ec6
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2019-01-14 14:48:22 -0800

    Add Employee class with #initialize and #bonus methods

---
+++ class_inheritance.rb
@@ -0,0 +1,32 @@
+class Employee
+  attr_reader :name, :title, :salary, :boss
+
+  def initialize(name, title, salary, boss)
+    @name = name
+    @title = title
+    @salary = salary
+    @boss = boss
+  end
+
+  def bonus(multiplier)
+    return salary * multiplier
+  end
+
+end
+
+class Manager < Employee
+
+  def initialize(name, title, salary, boss, employees)
+    super
+    @employees = employees
+  end
+
+  def bonus(multiplier)
+    total = 0
+    employees.each do |emp|
+      total += emp.salary
+    end
+    return total * multiplier
+  end
+
+end
