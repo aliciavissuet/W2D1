b20e0db86bf2a29dc468ee17c198f0606318ae35
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2019-01-14 16:27:54 -0800

    Add error handling in #move_piece

--- Chess/board.rb
+++ Chess/board.rb
@@ -12,16 +12,20 @@ class Board
     @chess_board.each_index do |idx1|
       @chess_board.each_index do |idx2|
         if idx1 == 0 || idx1 == 1 || idx1 == 6 || idx1 == 7
-          @chess_board[idx1][idx2] = Piece.new(idx1, idx2)
+          @chess_board[idx1][idx2] = Piece.new([idx1, idx2])
         else
-          @chess_board[idx1][idx2] = NullPiece.new(idx1, idx2)
+          @chess_board[idx1][idx2] = NullPiece.new([idx1, idx2])
         end
       end
     end
   end
 
   def move_piece(start_pos, end_pos)
-    raise NoPieceOnStart if self.[start_pos]
+    raise NoPieceOnStart if self[start_pos].is_a?(NullPiece)
+    raise EndPosOccupied unless self[end_pos].is_a?(NullPiece)
+    target_piece = self[start_pos]  # Instance of piece
+    target_piece.pos = end_pos
+    self[start_pos] = NullPiece.new(start_pos)
   end
 
   def [](pos)
