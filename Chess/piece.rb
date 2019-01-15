7d6fc1816ea6e77092b20f42d3b260ee301af8e0
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2019-01-14 16:17:50 -0800

    Update #initialize instance variable

--- Chess/piece.rb
+++ Chess/piece.rb
@@ -1,26 +1,11 @@
 class Piece
 
-  def initialize
+  def initialize(row, col)
+    # @color = color
+    # @board = board
+    @pos = [row, col]
   end
 
-end
-
-class Queen < Piece
-end
-
-class Rook < Piece
-end
-
-class Bishop < Piece
-end
-
-class Knight < Piece
-end
-
-class King < Piece
-end
-
-class Pawn < Piece
 end
 
 class NullPiece < Piece
