2fdaf3f926a179baa6589cc620eb5dbb19a089e1
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2019-01-14 16:16:57 -0800

    Add #[] syntactic sugar method

---
+++ Chess/board.rb
@@ -0,0 +1,34 @@
+require_relative 'piece'
+
+class Board
+  attr_reader :chess_board
+
+  def initialize
+    @chess_board = Array.new(8) {Array.new(8)}
+    fill_board
+  end
+
+  def fill_board
+    @chess_board.each_index do |idx1|
+      @chess_board.each_index do |idx2|
+        if idx1 == 0 || idx1 == 1 || idx1 == 6 || idx1 == 7
+          @chess_board[idx1][idx2] = Piece.new(idx1, idx2)
+        else
+          @chess_board[idx1][idx2] = NullPiece.new(idx1, idx2)
+        end
+      end
+    end
+  end
+
+  def move_piece(start_pos, end_pos)
+    raise NoPieceOnStart if self.[start_pos]
+  end
+
+  def [](pos)
+    @chess_board[pos[0]][pos[1]]
+  end
+
+end
+
+class NoPieceOnStart < StandardError; end
+class EndPosOccupied < StandardError; end
